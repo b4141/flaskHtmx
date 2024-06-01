02e898d0737537b89e5342c4d6c0a14fdd2b9c9e
b4141 <barikmail1@gmail.com>
2024-06-01 01:49:07 +0100

    complicating things that should not be complicated; jijna componentsgs

--- main.py
+++ main.py
@@ -5,24 +5,45 @@ import datetime
 app = Flask(__name__)
 
 
-class Customer:
-    def __init__(self, name):
-        self.name = name
-
-    def printName(self):
-        print(self.name)
-
-customers = []
-listOfNames = ["Delilah Osborne", "Augustus Wilkins", "Amalia Harris", "Samuel Herring", "Denver Rasmussen", "Will Huynh", "Oaklee Fitzgerald", "Peyton Bates", "Madilyn Jimenez", "Silas Burgess", "Emory Wade", "Jake Savage", "Louise Lynch", "Zane Saunders", "Meadow Meyers", "Julien Pacheco", "Paris Gordon", "Karter Wolfe", "Hallie Magana", "Rey Daniel", "Joy Holt", "Niko Allison", "Chelsea Flynn", "Kannon Parrish", "Tiana Tyler", "Emmitt Hansen", "Hope Hammond", "Francis Jenkins", "Rylee Caldwell", "Rylan Barr", "Noemi Berger", "Byron Trujillo", "Danielle Best", "Harlem Sosa", "Cassandra Long", "Jace Parrish", "Tiana Evans"]
-
-for i in range(len(listOfNames)):
-    customers.append(Customer(listOfNames[i]))
-
-class Transaction:
-    def __init__(self, customer, amount, date):
-        self.customer =  customer
-        self.amount = amount
-        self.date = date
+customers = [
+    {"name": "Delilah Osborne"},
+    {"name": "Augustus Wilkins"},
+    {"name": "Amalia Harris"},
+    {"name": "Samuel Herring"},
+    {"name": "Denver Rasmussen"},
+    {"name": "Will Huynh"},
+    {"name": "Oaklee Fitzgerald"},
+    {"name": "Peyton Bates"},
+    {"name": "Madilyn Jimenez"},
+    {"name": "Silas Burgess"},
+    {"name": "Emory Wade"},
+    {"name": "Jake Savage"},
+    {"name": "Louise Lynch"},
+    {"name": "Zane Saunders"},
+    {"name": "Meadow Meyers"},
+    {"name": "Julien Pacheco"},
+    {"name": "Paris Gordon"},
+    {"name": "Karter Wolfe"},
+    {"name": "Hallie Magana"},
+    {"name": "Rey Daniel"},
+    {"name": "Joy Holt"},
+    {"name": "Niko Allison"},
+    {"name": "Chelsea Flynn"},
+    {"name": "Kannon Parrish"},
+    {"name": "Tiana Tyler"},
+    {"name": "Emmitt Hansen"},
+    {"name": "Hope Hammond"},
+    {"name": "Francis Jenkins"},
+    {"name": "Rylee Caldwell"},
+    {"name": "Rylan Barr"},
+    {"name": "Noemi Berger"},
+    {"name": "Byron Trujillo"},
+    {"name": "Danielle Best"},
+    {"name": "Harlem Sosa"},
+    {"name": "Cassandra Long"},
+    {"name": "Jace Parrish"},
+    {"name": "Tiana Evans"}
+]
 
 transactions = []
 
@@ -30,19 +51,20 @@ def random_date(start, end):
     return datetime.date(randint(start, end), randint(1, 12), randint(1, 28))
 
 for i in range(100):
-    transactions.append(Transaction(customers[randint(0, len(customers)-1)], randint(20, 50),random_date(2019, 2024)))
-
+    transactions.append({"customer": customers[randint(0, len(customers)-1)], "amount": randint(20, 50),"date": random_date(2019, 2024)})
 
 @app.route("/")
 def index():
-    print(transactions[0].customer.name)
-    return render_template("index.html", title="index")
+    # print(transactions[0].customer.name)
+    return render_template("index.jinja", title="index")
 
 @app.route("/transactions", methods=['GET', 'POST'])
 def transactionsRoute():
     if request.method == 'POST':
-        return render_template("transactionsRoute/transactionsTable.html", transactions=transactions[:10])
-    return render_template("transactionsRoute/transactions.html", title="transactions", transactions=transactions)
+        sort_by = request.form.get('sort_by')
+        print("*****", sort_by)
+        return render_template("transactionsRoute/transactionsTable.jinja", transactions=transactions[:10], sortedBy={"value":"name", "ascending":True})
+    return render_template("transactionsRoute/transactions.jinja", title="transactions", transactions=transactions, sortedBy={"value": None})
 
 if __name__ == "__main__":
     app.run(debug=True)
